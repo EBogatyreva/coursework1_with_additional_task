600fc183b4b6f51abe601f7b54c6d8ce1a2e7c84
L <EBogatyrevaPK@yandex.ru>
2022-03-04 14:28:19 +1200

    Усложнённый уровень!

--- src/pro/sky/java/course1/Main.java
+++ src/pro/sky/java/course1/Main.java
@@ -1,8 +1,62 @@
 package pro.sky.java.course1;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        Employee employee1 = new Employee("Сотрудник1", 1, 1000);
+        System.out.println(employee1.getNameOfEmployee() + " " + employee1.getOffice() + " " + employee1.getSalary() + " " + employee1.getId());
+
+        Employee employee2 = new Employee("Сотрудник2", 3, 5_000);
+        System.out.println(employee2.getNameOfEmployee() + " " + employee2.getOffice() + " " + employee2.getSalary() + " " + employee2.getId());
+
+        Employee employee3 = new Employee("Сотрудник3", 2, 500);
+        System.out.println(employee3.getNameOfEmployee() + " " + employee3.getOffice() + " " + employee3.getSalary() + " " + employee3.getId());
+
+        Employee employee4 = new Employee("Сотрудник4", 4, 3_000);
+        System.out.println(employee4.getNameOfEmployee() + " " + employee4.getOffice() + " " + employee4.getSalary() + " " + employee4.getId());
+
+        employee1.setSalary(6_000);
+        employee1.setOffice(3);
+
+        Employee[] employee;
+        employee = new Employee[]{employee1, employee2, null, employee3, null, null, null, null, null, employee4};
+        String s = Arrays.toString(employee);
+        System.out.println(s);
+
+        System.out.println("Сумма затрат в месяц, на всех сотрудников: " + EmployeeService.sumSalary(employee) + " рублей");
+        System.out.println("Минимальная заработная плата: " + EmployeeService.minSalary(employee) + " рублей");
+        System.out.println("Максимальная заработная плата: " + EmployeeService.maxSalary(employee) + " рублей");
+        System.out.println("Средняя заработная плата: " + EmployeeService.averageSalary(employee) + " рублей");
+
+        EmployeeService.allNamesOfEmployee(employee);
+
+        System.out.println(" ");
+        System.out.println("Задачи повышенной сложности: ");
+        String s1 = Arrays.toString(employee);
+        System.out.println("Все сотрудники: " + s1);
+        EmployeeService.indexationByOfficeOnly(4, employee);
+        String s2 = Arrays.toString(employee);
+        System.out.println("Индексация только по номеру отдела: " + s2);
+
+        EmployeeService.indexation(employee);
+        String s3 = Arrays.toString(employee);
+        System.out.println("Индексация по всем отделам: " + s3);
+
+        EmployeeService.namesEmployeesOfOffice(3, employee);
+
+        System.out.println("Минимальная заработная плата по отделу: " + EmployeeService.minSalary(3, employee) + " рублей");
+        System.out.println("Максимальная заработная плата по отделу: " + EmployeeService.maxSalary(3, employee) + " рублей");
+        System.out.println("Сумма затрат в месяц на сотрудников отдела: " + EmployeeService.sumSalary(3, employee) + " рублей");
+        System.out.println("Средняя заработная плата по отделу: " + EmployeeService.averageSalary(3, employee) + " рублей");
+
+        EmployeeService.printAllEmployeeOfOffice(3, employee);
+
+        //найти по значению
+        EmployeeService.findSalaryLessNumber(2000, employee);
+        EmployeeService.findSalaryOverNumber(5000, employee);
+
+
     }
 }
+
